49db1e26c0e4daf5b63e3745ac1ab369929ff064
Iryna Klosheva <aripluss@i.ua>
2026-02-01 21:35:41 +0200

    fix: path

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "/src/styles/index.scss";
+import "./src/styles/index.scss";
 
 async function loadHTML(id, path) {
   const container = document.getElementById(id);
@@ -12,11 +12,11 @@ async function loadHTML(id, path) {
   container.innerHTML = html;
 }
 
-loadHTML("header", "/src/partials/header.html");
-loadHTML("hero", "/src/partials/main-page/hero.html");
-loadHTML("popular-designs", "/src/partials/main-page/popular-designs.html");
-loadHTML("how-it-works", "/src/partials/main-page/how-it-works.html");
-loadHTML("why-we", "/src/partials/main-page/why-we.html");
-loadHTML("steps-to-order", "/src/partials/main-page/steps-to-order.html");
-loadHTML("feedback", "/src/partials/main-page/feedback.html");
-loadHTML("footer", "/src/partials/footer.html");
+loadHTML("header", "./src/partials/header.html");
+loadHTML("hero", "./src/partials/main-page/hero.html");
+loadHTML("popular-designs", "./src/partials/main-page/popular-designs.html");
+loadHTML("how-it-works", "./src/partials/main-page/how-it-works.html");
+loadHTML("why-we", "./src/partials/main-page/why-we.html");
+loadHTML("steps-to-order", "./src/partials/main-page/steps-to-order.html");
+loadHTML("feedback", "./src/partials/main-page/feedback.html");
+loadHTML("footer", "./src/partials/footer.html");
