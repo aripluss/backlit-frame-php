6e6223980fa9bf0c340250bfcedd5370c2d1063e
Iryna Klosheva <aripluss@i.ua>
2026-02-17 00:10:47 +0200

    fix styles

--- src/partials/product-page/product-info.php
+++ src/partials/product-page/product-info.php
@@ -16,49 +16,50 @@ require __DIR__ . '/product-logic.php';
     <!-- Content -->
     <div class="product__content" data-id="<?= $product->id ?>"
       data-size="<?= htmlspecialchars($product->selectedSize) ?>"
-      data-custom="<?= $product->customDesign ? '1' : '0' ?>" data-base-price="<?= $productData['basePrice'] ?>"></div>
-    <div class="product__text-wrapper">
-      <h3 class="product__title">
-        <?= htmlspecialchars($product->title) ?>
-      </h3>
-      <p class="product__description">
-        <?= htmlspecialchars($productData['text']) ?>
-      </p>
-    </div>
+      data-custom="<?= $product->customDesign ? '1' : '0' ?>" data-base-price="<?= $productData['basePrice'] ?>">
 
-    <!-- Sizes -->
-    <div class="product__sizes">
-      <p class="product__sizes-title">Розмір</p>
-      <div class="product__sizes-list">
-        <?php foreach ($productData['sizes'] as $size): ?>
-          <button data-size="<?= $size ?>"
-            class="product__size btn btn--secondary <?= $product->selectedSize === $size ? 'product__size--active' : '' ?>">
-            <?= $size ?>
-          </button>
-        <?php endforeach; ?>
-      </div>
-    </div>
-
-    <!-- Checkbox -->
-    <label class="product__option">
-      <input type="checkbox" class="product__checkbox" <?= $product->customDesign ? 'checked' : '' ?> />
-      Індивідуальний макет (+350 грн)
-    </label>
-
-    <!-- Price -->
-    <div class="product__price-btn-wrapper">
-      <div class="product__price">
-        <span class="product__price-value">
-          <?= $price ?> грн
-        </span>
-        <span class="product__price-note">обраний розмір
-          <?= htmlspecialchars($product->selectedSize) ?>
-        </span>
+      <div class="product__text-wrapper">
+        <h3 class="product__title">
+          <?= htmlspecialchars($product->title) ?>
+        </h3>
+        <p class="product__description">
+          <?= htmlspecialchars($productData['text']) ?>
+        </p>
       </div>
 
-      <button class="product__cta btn btn--primary">Замовити світильник</button>
-    </div>
+      <!-- Sizes -->
+      <div class="product__sizes">
+        <p class="product__sizes-title">Розмір</p>
+        <div class="product__sizes-list">
+          <?php foreach ($productData['sizes'] as $size): ?>
+            <button data-size="<?= $size ?>"
+              class="product__size btn btn--secondary <?= $product->selectedSize === $size ? 'product__size--active' : '' ?>">
+              <?= $size ?>
+            </button>
+          <?php endforeach; ?>
+        </div>
+      </div>
 
-  </div>
+      <!-- Checkbox -->
+      <label class="product__option">
+        <input type="checkbox" class="product__checkbox" <?= $product->customDesign ? 'checked' : '' ?> />
+        Індивідуальний макет (+350 грн)
+      </label>
+
+      <!-- Price -->
+      <div class="product__price-btn-wrapper">
+        <div class="product__price">
+          <span class="product__price-value">
+            <?= $price ?> грн
+          </span>
+          <span class="product__price-note">обраний розмір
+            <?= htmlspecialchars($product->selectedSize) ?>
+          </span>
+        </div>
+
+        <button class="product__cta btn btn--primary">Замовити світильник</button>
+      </div>
+
+    </div>
   </div>
 </section>
